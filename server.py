9efe20c854ae329880e4e9dd4eac42086c37597d
dante <X_dante@qq.com>
2019-06-28 17:20:02 +0800

    'change'

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
+from apps import create_app
+
+app = create_app()
 
 APP_CONFIG={
     'host': '0.0.0.0',
     'port': 9001,
     'debug': True
 }
+
+
+if __name__ == '__main__':
+    app.run(**APP_CONFIG)
